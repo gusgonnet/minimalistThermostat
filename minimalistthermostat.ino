8910c4e039fa7f76ca8ae15cb3e1d0aa078060e1
ceajog <gustavo.gonnet@ericsson.com>
2016-02-22 10:25:01 -0500

    implemented minimum time for each state to protect on-off on the fan and the heating element
    version is now 0.9

--- minimalistthermostat.ino
+++ minimalistthermostat.ino
@@ -1,6 +1,3 @@
-//TODO:
-// have a minimun time for each state to protect on-off on the fan and the heating element
-
 //The MIT License (MIT)
 //Copyright (c) 2016 Gustavo Gonnet
 //
@@ -20,24 +17,24 @@
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
 // OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
+
 #include "application.h"
 #include "elapsedMillis.h"
 #include "PietteTech_DHT.h"
 #include "FiniteStateMachine.h"
-
 #define APP_NAME "Thermostat"
-// IO mapping
-// D0 : relay: fan
-// D1 : relay: heat
-// D2 : relay: cold
-// D3 : relay:
-// D4 : DHT22
-// D5 : unused
-// D6 : unused
-// D7 : unused
-// A0~A7 : unused
 
-//initialize states with proper enter, update and exit functions
+String VERSION = "Version 0.09";
+/*******************************************************************************
+ * changes in version 0.09:
+       * reorganized code to group functions
+       * added minimum time to protect on-off on the fan and the heating element
+          in function heatingUpdateFunction()
+*******************************************************************************/
+
+/*******************************************************************************
+ initialize FSM states with proper enter, update and exit functions
+*******************************************************************************/
 State initState = State( initEnterFunction, initUpdateFunction, initExitFunction );
 State idleState = State( idleEnterFunction, idleUpdateFunction, idleExitFunction );
 State heatingState = State( heatingEnterFunction, heatingUpdateFunction, heatingExitFunction );
@@ -45,23 +42,6 @@ State endOfCycleState = State( endOfCycleEnterFunction, endOfCycleUpdateFunction
 
 //initialize state machine, start in state: Idle
 FSM thermostatStateMachine = FSM(initState);
-
-String VERSION = "Version 0.07";
-int fan = D0;
-int heat = D1;
-int cold = D2;
-int fanTesting;
-int heatTesting;
-int coldTesting;
-
-// system defines
-#define DHTTYPE  DHT22                // Sensor type DHT11/21/22/AM2301/AM2302
-#define DHTPIN   4                    // Digital pin for communications
-#define DHT_SAMPLE_INTERVAL   30000   // Sample room temperature every 30 seconds
-void dht_wrapper(); // must be declared before the lib initialization
-PietteTech_DHT DHT(DHTPIN, DHTTYPE, dht_wrapper);
-bool bDHTstarted;       // flag to indicate we started acquisition
-elapsedMillis dhtSampleInterval;
 
 //milliseconds for the init cycle, so temperature samples get stabilized
 //this should be in the order of the 5 minutes: 5*60*1000==300000
@@ -73,6 +53,42 @@ elapsedMillis initTimer;
 #define END_OF_CYCLE_TIMEOUT 15000
 elapsedMillis endOfCycleTimer;
 
+//minimum number of milliseconds to leave the heating element on
+// to protect on-off on the fan and the heating element
+#define MINIMUM_ON_TIMEOUT 5000
+elapsedMillis minimumOnTimer;
+
+/*******************************************************************************
+ IO mapping
+*******************************************************************************/
+// D0 : relay: fan
+// D1 : relay: heat
+// D2 : relay: cold
+// D4 : DHT22
+// D3, D5~D7 : unused
+// A0~A7 : unused
+int fan = D0;
+int heat = D1;
+int cold = D2;
+//TESTING_HACK
+int fanTesting;
+int heatTesting;
+int coldTesting;
+
+/*******************************************************************************
+ DHT sensor
+*******************************************************************************/
+#define DHTTYPE  DHT22                // Sensor type DHT11/21/22/AM2301/AM2302
+#define DHTPIN   4                    // Digital pin for communications
+#define DHT_SAMPLE_INTERVAL   30000   // Sample room temperature every 30 seconds
+void dht_wrapper(); // must be declared before the lib initialization
+PietteTech_DHT DHT(DHTPIN, DHTTYPE, dht_wrapper);
+bool bDHTstarted;       // flag to indicate we started acquisition
+elapsedMillis dhtSampleInterval;
+
+/*******************************************************************************
+ thermostat related declarations
+*******************************************************************************/
 //temperature related variables - internal
 float targetTemp = 19.0;
 float currentTemp = 20.0;
@@ -83,54 +99,62 @@ String targetTempString = String(targetTemp); //String to store the target temp 
 String currentTempString = String(currentTemp); //String to store the sensor's temp so it can be exposed
 String currentHumidityString = String(currentHumidity); //String to store the sensor's humidity so it can be exposed
 
-//testing variables
+//TESTING_HACK
+// this allows me to system test the project
 bool testing = false;
 
+
+/*******************************************************************************
+ * Function Name  : setup
+ * Description    : this function runs once at system boot
+ *******************************************************************************/
 void setup() {
 
- //publish startup message with firmware version
- Particle.publish(APP_NAME, VERSION, 60, PRIVATE);
+  //pulish startup message with firmware version
+  Particle.publish(APP_NAME, VERSION, 60, PRIVATE);
 
- //declare and init pins
- pinMode(fan, OUTPUT);
- pinMode(heat, OUTPUT);
- pinMode(cold, OUTPUT);
- myDigitalWrite(fan, LOW);
- myDigitalWrite(heat, LOW);
- myDigitalWrite(cold, LOW);
+  //declare and init pins
+  pinMode(fan, OUTPUT);
+  pinMode(heat, OUTPUT);
+  pinMode(cold, OUTPUT);
+  myDigitalWrite(fan, LOW);
+  myDigitalWrite(heat, LOW);
+  myDigitalWrite(cold, LOW);
 
- //declare cloud variables
- //https://docs.particle.io/reference/firmware/photon/#particle-variable-
- //Currently, up to 10 cloud variables may be defined and each variable name is limited to a maximum of 12 characters
- if (Particle.variable("targetTemp", targetTempString)==false) {
-   Particle.publish(APP_NAME, "ERROR: Failed to register variable targetTemp", 60, PRIVATE);
- }
- if (Particle.variable("currentTemp", currentTempString)==false) {
-   Particle.publish(APP_NAME, "ERROR: Failed to register variable currentTemp", 60, PRIVATE);
- }
- if (Particle.variable("humidity", currentHumidityString)==false) {
-   Particle.publish(APP_NAME, "ERROR: Failed to register variable humidity", 60, PRIVATE);
- }
+  //declare cloud variables
+  //https://docs.particle.io/reference/firmware/photon/#particle-variable-
+  //Currently, up to 10 cloud variables may be defined and each variable name is limited to a maximum of 12 characters
+  if (Particle.variable("targetTemp", targetTempString)==false) {
+    Particle.publish(APP_NAME, "ERROR: Failed to register variable targetTemp", 60, PRIVATE);
+  }
+  if (Particle.variable("currentTemp", currentTempString)==false) {
+    Particle.publish(APP_NAME, "ERROR: Failed to register variable currentTemp", 60, PRIVATE);
+  }
+  if (Particle.variable("humidity", currentHumidityString)==false) {
+    Particle.publish(APP_NAME, "ERROR: Failed to register variable humidity", 60, PRIVATE);
+  }
 
- //declare cloud functions
- //https://docs.particle.io/reference/firmware/photon/#particle-function-
- //Currently the application supports the creation of up to 4 different cloud functions.
- //The length of the funcKey is limited to a max of 12 characters.
- // If you declare a function name longer than 12 characters the function will not be registered.
- //user functions
- if (Particle.function("setTargetTmp", setTargetTemp)==false) {
-    Particle.publish(APP_NAME, "ERROR: Failed to register function setTargetTemp", 60, PRIVATE);
- }
- //testing functions
- if (Particle.function("setTesting", setTesting)==false) {
-    Particle.publish(APP_NAME, "ERROR: Failed to register function setTesting", 60, PRIVATE);
- }
- if (Particle.function("setCurrTmp", setCurrentTemp)==false) {
-    Particle.publish(APP_NAME, "ERROR: Failed to register function setCurrentTemp", 60, PRIVATE);
- }
- if (Particle.function("getOutputs", getOutputs)==false) {
-    Particle.publish(APP_NAME, "ERROR: Failed to register function getOutputs", 60, PRIVATE);
- }
+  //declare cloud functions
+  //https://docs.particle.io/reference/firmware/photon/#particle-function-
+  //Currently the application supports the creation of up to 4 different cloud functions.
+  //The length of the funcKey is limited to a max of 12 characters.
+  // If you declare a function name longer than 12 characters the function will not be registered.
+  //user functions
+  if (Particle.function("setTargetTmp", setTargetTemp)==false) {
+     Particle.publish(APP_NAME, "ERROR: Failed to register function setTargetTemp", 60, PRIVATE);
+  }
+  //TESTING_HACK
+  if (Particle.function("setTesting", setTesting)==false) {
+     Particle.publish(APP_NAME, "ERROR: Failed to register function setTesting", 60, PRIVATE);
+  }
+  //TESTING_HACK
+  if (Particle.function("setCurrTmp", setCurrentTemp)==false) {
+     Particle.publish(APP_NAME, "ERROR: Failed to register function setCurrentTemp", 60, PRIVATE);
+  }
+  //TESTING_HACK
+  if (Particle.function("getOutputs", getOutputs)==false) {
+     Particle.publish(APP_NAME, "ERROR: Failed to register function getOutputs", 60, PRIVATE);
+  }
 
 }
 
@@ -138,9 +162,42 @@ void setup() {
 void dht_wrapper() { DHT.isrCallback(); }
 
 
+/*******************************************************************************
+ * Function Name  : loop
+ * Description    : this function runs continuously while the project is running
+ *******************************************************************************/
 void loop() {
+
+  //this function reads the temperature of the DHT sensor
   readTemperature();
+
+  //this function updates the FSM
+  // the FSM is the heart of the thermostat - all actions are defined by its states
   thermostatStateMachine.update();
+
+}
+
+/*******************************************************************************
+ * Function Name  : setTargetTemp
+ * Description    : sets the target temperature of the thermostat
+                    newTargetTemp has to be a valid float value, or no new target temp will be set
+ * Return         : 0, or -1 if it fails to convert the temp to float
+ *******************************************************************************/
+int setTargetTemp(String newTargetTemp)
+{
+  float tmpFloat = newTargetTemp.toFloat();
+  //update the target temp only in the case the conversion to float works
+  // (toFloat returns 0 if there is a problem in the conversion)
+  // sorry, if you wanted to set 0 as the target temp, you can't :)
+  if ( tmpFloat > 0 ) {
+    targetTemp = tmpFloat;
+    targetTempString = String(targetTemp);
+    Particle.publish(APP_NAME, "New target temp: " + targetTempString, 60, PRIVATE);
+    return 0;
+  } else {
+    Particle.publish(APP_NAME, "ERROR: Failed to set new target temp to " + newTargetTemp, 60, PRIVATE);
+    return -1;
+  }
 }
 
 /*******************************************************************************
@@ -151,6 +208,7 @@ void loop() {
  *******************************************************************************/
 int readTemperature() {
 
+  //TESTING_HACK
   //are we testing the app? then no need to acquire from the sensor
   if (testing) {
    return 0;
@@ -166,7 +224,7 @@ int readTemperature() {
 
   // start the sample
   if (!bDHTstarted) {
-    DHT.acquire();
+    DHT.acquireAndWait(5);
     bDHTstarted = true;
   }
 
@@ -214,7 +272,14 @@ int publishTemperature( float temperature, float humidity ) {
 
 }
 
-//utility functions
+
+/*******************************************************************************
+********************************************************************************
+********************************************************************************
+ FINITE STATE MACHINE FUNCTIONS
+********************************************************************************
+********************************************************************************
+*******************************************************************************/
 void initEnterFunction(){
  Particle.publish(APP_NAME, "initEnterFunction", 60, PRIVATE);
  //start the timer of this cycle
@@ -252,12 +317,21 @@ void heatingEnterFunction(){
  myDigitalWrite(fan, HIGH);
  myDigitalWrite(heat, HIGH);
  myDigitalWrite(cold, LOW);
+
+ //start the minimum timer of this cycle
+ minimumOnTimer = 0;
 }
 void heatingUpdateFunction(){
- if ( currentTemp >= (targetTemp + margin) ) {
-  Particle.publish(APP_NAME, "Desired temperature reached", 60, PRIVATE);
-  thermostatStateMachine.transitionTo(endOfCycleState);
- }
+  //is minimum time up?
+  if (minimumOnTimer < MINIMUM_ON_TIMEOUT) {
+    //not yet, so get out of here
+    return;
+  }
+
+  if ( currentTemp >= (targetTemp + margin) ) {
+    Particle.publish(APP_NAME, "Desired temperature reached", 60, PRIVATE);
+    thermostatStateMachine.transitionTo(endOfCycleState);
+  }
 }
 void heatingExitFunction(){
  Particle.publish(APP_NAME, "heatingExitFunction", 60, PRIVATE);
@@ -286,9 +360,20 @@ void endOfCycleExitFunction(){
  Particle.publish(APP_NAME, "endOfCycleExitFunction", 60, PRIVATE);
 }
 
+
+/*******************************************************************************
+********************************************************************************
+********************************************************************************
+ TESTING HACKS
+********************************************************************************
+********************************************************************************
+*******************************************************************************/
+
 /*******************************************************************************
  * Function Name  : setTesting
- * Description    : sets the testing variable to true
+ * Description    : sets the testing variable to true - this enables me to override
+                     the temperature read by the DHT sensor.
+                    this is a hack that allows me to system test the project
  * Return         : 0
  *******************************************************************************/
 int setTesting(String dummy)
@@ -300,6 +385,7 @@ int setTesting(String dummy)
 /*******************************************************************************
  * Function Name  : getOutputs
  * Description    : returns the outputs so we can test the program
+                    this is a hack that allows me to system test the project
  * Return         : returns the outputs
  *******************************************************************************/
 int getOutputs(String dummy)
@@ -314,7 +400,8 @@ int getOutputs(String dummy)
  * Function Name  : setCurrentTemp
  * Description    : sets the current temperature of the thermostat
                     newCurrentTemp has to be a valid float value, or no new current temp will be set
- * Return         : 0, or -1 if it fails to convert the temp to float
+                    this is a hack that allows me to system test the project
+* Return         : 0, or -1 if it fails to convert the temp to float
  *******************************************************************************/
 int setCurrentTemp(String newCurrentTemp)
 {
@@ -335,28 +422,11 @@ int setCurrentTemp(String newCurrentTemp)
 }
 
 /*******************************************************************************
- * Function Name  : setTargetTemp
- * Description    : sets the target temperature of the thermostat
-                    newTargetTemp has to be a valid float value, or no new target temp will be set
- * Return         : 0, or -1 if it fails to convert the temp to float
+ * Function Name  : getOutputs
+ * Description    : returns the outputs so we can test the program
+                    this is a hack that allows me to system test the project
+ * Return         : returns the outputs
  *******************************************************************************/
-int setTargetTemp(String newTargetTemp)
-{
-  float tmpFloat = newTargetTemp.toFloat();
-  //update the target temp only in the case the conversion to float works
-  // (toFloat returns 0 if there is a problem in the conversion)
-  // sorry, if you wanted to set 0 as the target temp, you can't :)
-  if ( tmpFloat > 0 ) {
-    targetTemp = tmpFloat;
-    targetTempString = String(targetTemp);
-    Particle.publish(APP_NAME, "New target temp: " + targetTempString, 60, PRIVATE);
-    return 0;
-  } else {
-    Particle.publish(APP_NAME, "ERROR: Failed to set new target temp to " + newTargetTemp, 60, PRIVATE);
-    return -1;
-  }
-}
-
 void myDigitalWrite( int input, int status){
   digitalWrite(input, status);
   if (input == fan){
