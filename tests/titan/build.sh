7be2fcec461ab478d52b7b2cddfb5b54cef16557
ceajog <gustavo.gonnet@ericsson.com>
2016-04-02 00:26:50 -0400

    updates with improvements after writing the hackster article
    https://www.hackster.io/gusgonnet/how-to-test-your-projects-with-titan-a633f2

--- tests/titan/build.sh
+++ tests/titan/build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+_script_dir=$( dirname $( readlink -f "$0" ) )
+SRC_DIR="${_script_dir}/."
+BIN_DIR="bin"
+
+mkdir -p ${BIN_DIR}
+cd ${BIN_DIR}
+
+for SRC in $(find ${SRC_DIR} -type f)
+do
+    ln -s ${SRC} .
+done
 
 makefilegen -g -f -e minimalistThermostat -o . *
 make
--- tests/titan/run.sh
+++ tests/titan/run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+_script_dir=$( dirname $( readlink -f "$0" ) )
+BIN_DIR="bin"
+
+cd ${BIN_DIR}
 ttcn3_start minimalistThermostat minimalistThermostat.cfg
